f98bca91229df7b636a6b93c30633b6193d9bdd0
Andrew Yatsutko <eitherwiser@gmail.com>
2022-02-11 11:53:46 +0200

    created react app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Suspense, useEffect, lazy } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import './App.css';
+
+export default function App() {
+  return <h1>Hello</h1>;
+}
