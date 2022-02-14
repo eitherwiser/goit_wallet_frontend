ea89a8144245696af23a5bd15046f16334ee95df
Olya Kikot <kikotolya1989@gmail.com>
2022-02-14 15:07:36 +0200

    adds chart

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { Suspense, useEffect, lazy } from 'react';
+import React, { Suspense, useEffect, lazy } from "react";
 // import { useSelector, useDispatch } from "react-redux";
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import Container from 'components/Container/';
-import Header from 'components/Header';
-import DashboardPage from 'pages/DashboardPage';
-import './App.css';
-import LoginPage from 'pages/LoginPage';
-import RegisterPage from 'pages/RegisterPage';
+import Container from "components/Container/";
+import Header from "components/Header";
+import DashboardPage from "pages/DashboardPage";
+import "./App.css";
+import LoginPage from "pages/LoginPage";
+import RegisterPage from "pages/RegisterPage";
+import StatisticsPages from "pages/StatisticsPages/StatisticsPages";
 
 export default function App() {
   return (
     <>
       <Container>
         <Header />
+      </Container>
+      <Container>
+        <StatisticsPages />
       </Container>
 
       <Routes>
