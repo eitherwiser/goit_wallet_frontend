f7df2c2ee9052db9290dc639138654112fcf3d6d
Serhii-Zvieriev <zacjok@gmail.com>
2022-02-16 00:17:57 +0200

    add Table on mobile

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import StatisticsPages from "pages/StatisticsPages/StatisticsPages";
 import React, { Suspense, useEffect, lazy } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Media from "react-media";
 import { ToastContainer } from "react-toastify";
 import Container from "components/Container/";
 import Header from "components/Header";
 import DashboardPage from "pages/DashboardPage/DashboardPage";
 import Statistics from "./pages/StatisticsPages/StatisticsPages";
-import TransactionTable from "./components/TransactionTable/TransactionTable";
 import CurrencyTable from "./components/Currency/Currency";
+import Table from "components/TransactionTable/Table";
+import useWindowSize from "./hooks/useWindowSize";
 
 import "./App.css";
 import LoginPage from "pages/LoginPage";
@@ -19,6 +19,7 @@ import "react-toastify/dist/ReactToastify.min.css";
 
 export default function App() {
   const isAuth = useSelector(getAuth);
+  const size = useWindowSize();
   return (
     <>
       <ToastContainer position="top-right" autoClose={3000} />
@@ -42,27 +43,18 @@ export default function App() {
           }
         >
           {/* <Route path="diagram" element={<Statistics />} /> */}
-          <Route
-            path="home"
-            element={
-              <Media
-                query="(min-width: 768px)"
-                render={() => <TransactionTable />}
-              />
-            }
-          />
+          <Route path="home" element={<Table />} />
           {/*Вместо текста компонент кружочка */}
           <Route path="diagram" element={<span>Кружок со статистикой</span>} />
-          {/*Вместо текста компонент приватбанка */}
-          {/* <Route path="exchangeRates" element={<span>Курс валют</span>} />  */}
 
           <Route
             path="exchangeRates"
             element={
-              <Media
-                query="(min-width: 768px)"
-                render={() => <CurrencyTable />}
-              />
+              size.width < 768 ? (
+                <CurrencyTable />
+              ) : (
+                <Navigate replace to="/home" />
+              )
             }
           />
         </Route>
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,17 +1,13 @@
 import Media from "react-media";
 import { Outlet } from "react-router-dom";
-
-// import Container from "components/Container/Container";
 import Navigation from "components/Navigation/Navigation";
 import Balance from "components/Balance/Balance";
-// import TransactionTable from "components/TransactionTable/TransactionTable";
 import CurrencyTable from "components/Currency/Currency";
 
 import s from "./DashboardPage.module.css";
 
 export default function DashboardPage() {
   return (
-    // <Container>
     <div className={s.dashboardPage}>
       <div className={s.container}>
         <div className={s.nav}>
@@ -19,12 +15,8 @@ export default function DashboardPage() {
           <Balance />
         </div>
         <Media query="(min-width: 768px)" render={() => <CurrencyTable />} />
-        {/* <CurrencyTable /> */}
       </div>
-
-      {/* <Media query="(min-width: 768px)" render={() => <TransactionTable />} /> */}
       <Outlet />
     </div>
-    // </Container>
   );
 }
