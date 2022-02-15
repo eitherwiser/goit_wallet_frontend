a15e78a274d0b59d031ced5e2c2a278f8dcfd192
Andrew Yatsutko <eitherwiser@gmail.com>
2022-02-15 23:30:09 +0200

    fix-statistic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import StatisticsPages from "pages/StatisticsPages/StatisticsPages";
-import React, { Suspense, useEffect, lazy } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Media from "react-media";
-import { ToastContainer } from "react-toastify";
-import Container from "components/Container/";
-import Header from "components/Header";
-import DashboardPage from "pages/DashboardPage/DashboardPage";
-import Statistics from "./pages/StatisticsPages/StatisticsPages";
-import TransactionTable from "./components/TransactionTable/TransactionTable";
-import CurrencyTable from "./components/Currency/Currency";
+import StatisticsPages from 'pages/StatisticsPages/StatisticsPages';
+import React, { Suspense, useEffect, lazy } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Media from 'react-media';
+import { ToastContainer } from 'react-toastify';
+import Container from 'components/Container/';
+import Header from 'components/Header';
+import DashboardPage from 'pages/DashboardPage/DashboardPage';
+import Statistics from './pages/StatisticsPages/StatisticsPages';
+import TransactionTable from './components/TransactionTable/TransactionTable';
+import CurrencyTable from './components/Currency/Currency';
 
-import "./App.css";
-import LoginPage from "pages/LoginPage";
-import RegisterPage from "pages/RegisterPage";
-import { getAuth } from "redux/auth/auth-selectors";
-import "react-toastify/dist/ReactToastify.min.css";
+import './App.css';
+import LoginPage from 'pages/LoginPage';
+import RegisterPage from 'pages/RegisterPage';
+import { getAuth } from 'redux/auth/auth-selectors';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 export default function App() {
   const isAuth = useSelector(getAuth);
@@ -41,7 +41,7 @@ export default function App() {
             // !isAuth && <Navigate replace to="/login" />
           }
         >
-          {/* <Route path="diagram" element={<Statistics />} /> */}
+          <Route path="diagram" element={<Statistics />} />
           <Route
             path="home"
             element={
