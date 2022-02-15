b5a3a5b0bddc9984907fe1b702654d2421dc8d7b
Serhii-Zvieriev <zacjok@gmail.com>
2022-02-15 20:16:04 +0200

    done 50%

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,24 +1,30 @@
 import Media from "react-media";
+import { Outlet } from "react-router-dom";
 
-import Container from "components/Container/Container";
+// import Container from "components/Container/Container";
 import Navigation from "components/Navigation/Navigation";
 import Balance from "components/Balance/Balance";
-import TransactionTable from "components/TransactionTable/TransactionTable";
+// import TransactionTable from "components/TransactionTable/TransactionTable";
 import CurrencyTable from "components/Currency/Currency";
 
 import s from "./DashboardPage.module.css";
 
 export default function DashboardPage() {
   return (
-    <Container>
-      <div className={s.dashboardPage}>
-        <Navigation />
-        <Balance />
-        <CurrencyTable />
-        <div>Приват Банк</div>
-
-        <Media query="(min-width: 768px)" render={() => <TransactionTable />} />
+    // <Container>
+    <div className={s.dashboardPage}>
+      <div className={s.container}>
+        <div className={s.nav}>
+          <Navigation />
+          <Balance />
+        </div>
+        <Media query="(min-width: 768px)" render={() => <CurrencyTable />} />
+        {/* <CurrencyTable /> */}
       </div>
-    </Container>
+
+      {/* <Media query="(min-width: 768px)" render={() => <TransactionTable />} /> */}
+      <Outlet />
+    </div>
+    // </Container>
   );
 }
