546d282ed15a78db5312d27b9134b5b1d370bb9e
Olya Kikot <kikotolya1989@gmail.com>
2022-02-16 20:17:10 +0200

    new

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,73 +1,74 @@
-import React from 'react';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
-import s from "./Chart.module.css"
-import DiagramTab from "../../components/DiagramTab/DiagramTab"
-
-
+import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+import s from "./Chart.module.css";
+import DiagramTab from "../../components/DiagramTab/DiagramTab";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-
- const data = {
-  labels: ['Основные рассходы ', 'Продукты', 'Машина', 'Забота о себе', 'Забота о детях', 'Товары для дома', 'Образование', 'Досуг', 'Другие рассходы'],
+const data = {
+  labels: [
+    "Основные расходы ",
+    "Продукты",
+    "Машина",
+    "Забота о себе",
+    "Забота о детях",
+    "Товары для дома",
+    "Образование",
+    "Досуг",
+    "Другие расходы",
+  ],
 
   datasets: [
     {
-      label: '# of Votes',
+      label: "# of Votes",
       data: [8700, 3800, 1500, 800, 2208, 300, 3400, 1230, 610],
       backgroundColor: [
-        '#FED057',
-        '#FFD8D0',
-        '#FD9498',
-        '#C5BAFF',
-        '#6E78E8',
-        '#4A56E2',
-        '#81E1FF',
-        '#24CCA7', 
-        '#00AD84',
+        "#FED057",
+        "#FFD8D0",
+        "#FD9498",
+        "#C5BAFF",
+        "#6E78E8",
+        "#4A56E2",
+        "#81E1FF",
+        "#24CCA7",
+        "#00AD84",
       ],
-
 
       borderWidth: 0,
       cutout: 110,
-   
-      
     },
-
-
   ],
-
 };
 
 const options = {
-    plugins: {
-
-      title: {
-        display: true,
-        text: '24000',
-       
-       
-      },
-        legend: {
-            display: false,
-
-        }
+  plugins: {
+    title: {
+      display: true,
+      text: "24000",
     },
-
-}
+    legend: {
+      display: false,
+    },
+  },
+};
 
 export default function Chart() {
   return (
-  <>
-
-
-<div className={s.wrapper}>    <p className={s.mainTitte}>Статистика</p>
-<div className={s.rightContainer}><div className={s.chart}>  <Doughnut data={data} options={options}/></div>
-<div><DiagramTab/></div></div></div>
-
-
-  </>
-  )
+    <>
+      <div className={s.wrapper}>
+        {" "}
+        <p className={s.mainTitle}>Статистика</p>
+        <div className={s.rightContainer}>
+          <div className={s.chart}>
+            {" "}
+            <Doughnut data={data} options={options} />
+          </div>
+          <div>
+            <DiagramTab />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-
