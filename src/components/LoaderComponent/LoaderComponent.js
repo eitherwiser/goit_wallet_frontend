89a6303d32be44fec4962b70f61afbdfeb28ae7e
Andrew Yatsutko <eitherwiser@gmail.com>
2022-02-16 03:12:53 +0200

    ...

--- src/components/LoaderComponent/LoaderComponent.js
+++ src/components/LoaderComponent/LoaderComponent.js
@@ -1,7 +1,6 @@
-import { Audio } from "react-loader-spinner";
-import "../../index.css";
-// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { Audio } from 'react-loader-spinner';
+import '../../index.css';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 // import s from "./LoaderComponent.module.css";
 
 export default function LoaderComponent() {
