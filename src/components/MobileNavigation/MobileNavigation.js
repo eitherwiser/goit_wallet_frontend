bf83fbf4a01ab4e04b93aaf6adfb6245fbd32032
OksanaBakl <oksanamolesha@ukr.net>
2022-02-17 13:56:26 +0200

    added Mobile Navigation

---
+++ src/components/MobileNavigation/MobileNavigation.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import path from '../../routes_path';
+
+import styles from './MobileNavigation.module.css';
+import sprite from '../../images/svg_sprite.svg';
+
+export default function MobileNavigation() {
+  return (
+    <>
+      <nav className={styles.imgsmallnav}>
+        <NavLink
+          className={styles.imgsmall}
+          activeClassName={styles.imgsmallactive}
+          exact
+          to={path.dashboardPage}
+        >
+          <svg height="38px" width="38px">
+            <use href={sprite + '#icon-home'}></use>
+          </svg>
+        </NavLink>
+        <NavLink
+          className={styles.imgsmall}
+          activeClassName={styles.imgsmallactive}
+          exact
+          to={path.statistic}
+        >
+          <svg height="38px" width="38px">
+            <use href={sprite + '#icon-statistics'}></use>
+          </svg>
+        </NavLink>
+        <NavLink
+          className={styles.imgsmall}
+          activeClassName={styles.imgsmallactive}
+          exact
+          to="/exchangeRates"
+        >
+          <svg height="38px" width="38px">
+            <use href={sprite + '#icon-currency'}></use>
+          </svg>
+        </NavLink>
+      </nav>
+    </>
+  );
+}
---
+++ src/routes_path.js
@@ -0,0 +1,9 @@
+const path = {
+  registrationPage: '/register',
+  logInPage: '/login',
+  dashboardPage: '/home',
+  statistic: '/diagram',
+  exchangeRates: '/exchangeRates',
+};
+
+export default path;
