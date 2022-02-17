3af5e24cc65f6955757dc0e7d9effba75bd2f8ad
Olya Kikot <kikotolya1989@gmail.com>
2022-02-17 13:19:49 +0200

    отрисовка по данным

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -1,6 +1,58 @@
 import React from "react";
 import s from "./DiagramTab.module.css";
 import TableFilters from "components/Select/Select";
+import Category from "components/category/category";
+
+
+const obj={
+  "category": [
+      {
+          "id": "321321321",
+          "name": "Еда поменял вручную",
+          "isInc": false,
+          "color": "#ffffff",
+          "isActive": true,
+          "total": 10
+      },
+      {
+          "id": "387721321",
+          "name": "Дети",
+          "isInc": false,
+          "color": "#ff22ff",
+          "isActive": true,
+          "total": 20
+      },
+      {
+          "id": "321320909",
+          "name": "Развитие",
+          "isInc": false,
+          "color": "#33ffff",
+          "isActive": true,
+          "total": 30
+      },
+      {
+          "id": "771321321",
+          "name": "Остальное",
+          "isInc": false,
+          "color": "#f45454",
+          "isActive": true,
+          "total": 30
+      },
+      {
+          "id": "321328821",
+          "name": "Налоги",
+          "isInc": false,
+          "color": "#ff2345",
+          "isActive": true,
+          "total": 40
+      }
+  ],
+  "total": {
+      "Expence": 130,
+      "Income": 50000
+  }
+}
+
 
 export default function DiagramTab() {
 
@@ -10,29 +62,16 @@ export default function DiagramTab() {
       <div className={s.container}>
         <p className={s.title}>Категория</p> <p className={s.title}>Сумма</p>
       </div>
-      <div className={s.containerTab}>
-        <div className={s.leftContainer}>
-          <span
-            style={{
-              display: "block",
-              width: "24px",
-              height: "24px",
-              backgroundColor: "#000",
-              marginRight: "16px",
-            }}
-          ></span>
-          <p className={s.name}>Основные расходы:</p>
-        </div>
-        <p className={s.name}>3000</p>
-      </div>
+    
+     <Category/>
 
       <div className={s.containerSum}>
         <p className={s.titleSum}>Расходы</p>
-        <span className={s.costs}>200</span>
+        <span className={s.costs}>{obj.total.Expence}</span>
       </div>
       <div className={s.containerSum}>
         <p className={s.titleSum}>Доходы</p>
-        <span className={s.income}>100</span>
+        <span className={s.income}>{obj.total.Income}</span>
       </div>
     </>
   );
