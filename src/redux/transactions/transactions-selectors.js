298735f9b7e2c0eb2d63ef51ac8125a2acb2b38e
AnatoliyDanets <danetsxp@gmail.com>
2022-02-21 23:43:38 +0200

    fix balance

--- src/redux/transactions/transactions-selectors.js
+++ src/redux/transactions/transactions-selectors.js
@@ -1,3 +1,3 @@
 export const getTransactions = (state) => state.transactions.items;
 export const getLoading = (state) => state.transactions.isLoading;
-export const balanceTransaction = (state) => state.transactions.balance;
+export const getBalance = (state) => state.transactions.balance;
