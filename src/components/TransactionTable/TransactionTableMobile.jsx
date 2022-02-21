b15ed48e312c9e954b6457a116ad9cde9207d123
OksanaBakl <oksanamolesha@ukr.net>
2022-02-21 13:32:18 +0200

    fixed BtnAddTransaction

--- src/components/TransactionTable/TransactionTableMobile.jsx
+++ src/components/TransactionTable/TransactionTableMobile.jsx
@@ -1,16 +1,17 @@
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableRow from "@mui/material/TableRow";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { useSelector } from "react-redux";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useSelector } from 'react-redux';
+import ButtonAddTransaction from '../ButtonAddTransactions/ButtonAddTransactions';
 
-import { getTransactionCategories } from "../../redux/auth/auth-selectors";
+import { getTransactionCategories } from '../../redux/auth/auth-selectors';
 
-import NoTransactions from "../NoTransactions/NoTransactions";
+import NoTransactions from '../NoTransactions/NoTransactions';
 
-import dateConverter from "../../services/dateConverter";
-import createData from "../../services/createData";
+import dateConverter from '../../services/dateConverter';
+import createData from '../../services/createData';
 
 // import s from "./TransactionTable.module.css";
 
@@ -22,7 +23,7 @@ const theme = createTheme({
         root: {
           // Some CSS
           tbody: {
-            backgroundColor: "#fff",
+            backgroundColor: '#fff',
           },
         },
       },
@@ -33,12 +34,12 @@ const theme = createTheme({
 export default function TransactionTableMobile({ transactions }) {
   const transactionCategories = useSelector(getTransactionCategories);
 
-  const column = transactions.map((trans) => {
-    const isIncome = trans.isIncome ? "+" : "-";
+  const column = transactions.map(trans => {
+    const isIncome = trans.isIncome ? '+' : '-';
     const fullDate = dateConverter(trans.date);
 
     const transactionName = transactionCategories.find(
-      (el) => el.id === trans.categoryId
+      el => el.id === trans.categoryId,
     );
 
     const arrCol = createData(
@@ -47,79 +48,87 @@ export default function TransactionTableMobile({ transactions }) {
       transactionName.name,
       trans.comment,
       trans.amount.toFixed(2),
-      trans.balance.toFixed(2)
+      trans.balance.toFixed(2),
     );
 
     return arrCol;
   });
 
   if (column[0] === undefined) {
-    return <NoTransactions />;
+    return (
+      <>
+        <NoTransactions />,
+        <ButtonAddTransaction />
+      </>
+    );
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      {column.map((col, idx) => (
-        <Table
-          key={idx}
-          sx={{
-            maxWidth: 480,
-            mb: 10,
-            borderCollapse: "initial",
-            borderLeft:
-              col.type === "-" ? "5px solid #24CCA7" : "5px solid #FF6596",
-            borderRadius: "10px",
-            margin: "0 auto 10px",
-          }}
-        >
-          <TableBody>
-            <TableRow
-              sx={{
-                "& .MuiTableCell-root:last-of-type": {
-                  borderTopRightRadius: "10px",
-                },
-              }}
-            >
-              <TableCell align="left">Дата</TableCell>
-              <TableCell align="right">{col.name}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">Тип</TableCell>
-              <TableCell align="right">{col.type}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">Категория</TableCell>
-              <TableCell align="right">{col.category}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">Коментарий</TableCell>
-              <TableCell align="right">{col.comment}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">Сумма</TableCell>
-              <TableCell
+    <>
+      <ThemeProvider theme={theme}>
+        {column.map((col, idx) => (
+          <Table
+            key={idx}
+            sx={{
+              maxWidth: 480,
+              mb: 10,
+              borderCollapse: 'initial',
+              borderLeft:
+                col.type === '-' ? '5px solid #24CCA7' : '5px solid #FF6596',
+              borderRadius: '10px',
+              margin: '0 auto 10px',
+            }}
+          >
+            <TableBody>
+              <TableRow
                 sx={{
-                  color: col.type === "-" ? "#24CCA7" : "#FF6596",
-                  fontWeight: 700,
+                  '& .MuiTableCell-root:last-of-type': {
+                    borderTopRightRadius: '10px',
+                  },
                 }}
-                align="right"
               >
-                {col.amount}
-              </TableCell>
-            </TableRow>
-            <TableRow
-              sx={{
-                "& .MuiTableCell-root:last-of-type": {
-                  borderBottomRightRadius: "10px",
-                },
-              }}
-            >
-              <TableCell align="left">Баланс</TableCell>
-              <TableCell align="right">{col.balance}</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      ))}
-    </ThemeProvider>
+                <TableCell align="left">Дата</TableCell>
+                <TableCell align="right">{col.name}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell align="left">Тип</TableCell>
+                <TableCell align="right">{col.type}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell align="left">Категория</TableCell>
+                <TableCell align="right">{col.category}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell align="left">Коментарий</TableCell>
+                <TableCell align="right">{col.comment}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell align="left">Сумма</TableCell>
+                <TableCell
+                  sx={{
+                    color: col.type === '-' ? '#24CCA7' : '#FF6596',
+                    fontWeight: 700,
+                  }}
+                  align="right"
+                >
+                  {col.amount}
+                </TableCell>
+              </TableRow>
+              <TableRow
+                sx={{
+                  '& .MuiTableCell-root:last-of-type': {
+                    borderBottomRightRadius: '10px',
+                  },
+                }}
+              >
+                <TableCell align="left">Баланс</TableCell>
+                <TableCell align="right">{col.balance}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        ))}
+      </ThemeProvider>
+      <ButtonAddTransaction />
+    </>
   );
 }
