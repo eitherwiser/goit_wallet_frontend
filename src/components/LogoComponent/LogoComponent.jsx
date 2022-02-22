558e2db03bcaaeb4a14bc0de2e6f1de3ba0193df
Andrew Yatsutko <eitherwiser@gmail.com>
2022-02-22 22:34:51 +0200

    fixed background and stiles

--- src/components/LogoComponent/LogoComponent.jsx
+++ src/components/LogoComponent/LogoComponent.jsx
@@ -1,10 +1,10 @@
-import { useMediaQuery } from 'react-responsive';
-import s from './LogoComponent.module.css';
-import { ReactComponent as LogoMobile } from '../../images/logo-form/logoMobile.svg';
-import { ReactComponent as Logo } from '../../images/logo-form/logo.svg';
+import { useMediaQuery } from "react-responsive";
+import s from "./LogoComponent.module.css";
+import { ReactComponent as LogoMobile } from "images/logo-form/logoMobile.svg";
+import { ReactComponent as Logo } from "images/logo-form/logo.svg";
 
 export default function LogoComponent() {
-  const isMobileOrTablet = useMediaQuery({ query: '(min-width: 768px)' });
+  const isMobileOrTablet = useMediaQuery({ query: "(min-width: 768px)" });
   return (
     <>
       {isMobileOrTablet ? (
