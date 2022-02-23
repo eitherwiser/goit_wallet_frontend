a1db0582dc008cc9cd3400e04a53b8222f1145ff
Andrew Yatsutko <eitherwiser@gmail.com>
2022-02-23 19:56:48 +0200

    fix markup fo login and rgistration

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -3,17 +3,19 @@ import { useMediaQuery } from "react-responsive";
 import RegisterForm from "components/RegisterForm/RegisterForm";
 
 function RegisterPage() {
-  const isLarge = useMediaQuery({ query: "(min-width: 1240px)" });
+  const isLarge = useMediaQuery({ query: "(min-width: 1280px)" });
   return (
-    <div className={s.container}>
-      <div className={s.wrapper}>
-        <h1 className={s.title}>Finance App</h1>
+    <>
+      {isLarge && <div className={s.content_filter}></div>}
+      <div className={s.container}>
+        <div className={s.wrapper}>
+          <h1 className={s.title}>Finance App</h1>
+        </div>
+        <div className={s.content_container}>
+          <RegisterForm />
+        </div>
       </div>
-      <div className={s.content_container}>
-        {isLarge && <div className={s.content_filter}></div>}
-        <RegisterForm />
-      </div>
-    </div>
+    </>
   );
 }
 
